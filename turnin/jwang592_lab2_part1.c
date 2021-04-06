7409f982ea977b2359541cf541b6da5283bc14c3
justindwang <justindwang@yahoo.com>
2021-04-06 09:56:59 -0700

    Completed part 1

---
+++ turnin/jwang592_lab2_part1.c
@@ -0,0 +1,36 @@
+/*	Author: jwang592
+ *  Partner(s) Name: Justin Wang
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+
+
+	while(1) {
+		tmpA = PINA & 0x03;
+
+
+		if(tmpA == 0x01) {
+			tmpB = 0x01;
+		}
+		else {
+			tmpB = 0x00;
+		}
+		PORTB = tmpB;
+
+	}
+	return 0;
+}
