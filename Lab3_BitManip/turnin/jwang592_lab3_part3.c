fb77c2667483fbb8cb1a52504c54fcc937c1de34
justindwang <justindwang@yahoo.com>
2021-04-06 21:29:47 -0700

    Completed part 3

---
+++ Lab3_BitManip/turnin/jwang592_lab3_part3.c
@@ -0,0 +1,49 @@
+/*	Author: jwang592
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab 3  Exercise 3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+        DDRC = 0xFF; PORTC = 0x00;
+        unsigned char tmpA, tmpC = 0x00;
+
+        while (1) {
+                tmpA = PINA & 0x0F;
+		tmpC = 0x40;
+                if(tmpA > 0){
+			tmpC = tmpC | 0x20;}
+		if(tmpA > 2){
+			tmpC = tmpC | 0x10;}
+		if(tmpA > 4){
+			tmpC = tmpC | 0x08;}
+		if(tmpA > 6){
+			tmpC = tmpC | 0x04;}
+		if(tmpA > 9){
+			tmpC = tmpC | 0x02;}
+		if(tmpA > 12){
+			tmpC = tmpC | 0x01;}
+		if( tmpA > 5){
+			tmpC = tmpC & 0xBF;}
+
+		tmpA = PINA & 0x70;
+		if((tmpA & 0x10) &&0x01){
+			if((tmpA & 0x20) && 0x01){
+				if(!((tmpA & 0x40) && 0x01)){
+					tmpC = tmpC | 0x80;
+				}
+			}
+		}
+		PORTC = tmpC;
+        }
+	return 0;
+}
