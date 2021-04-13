811eedd52dfb381ba62fa8bf49b2d8f4db838886
justindwang <justindwang@yahoo.com>
2021-04-12 18:36:48 -0700

    Fixed part 1

--- Lab4_SMs/turnin/jwang592_lab4_part1.c
+++ Lab4_SMs/turnin/jwang592_lab4_part1.c
@@ -13,7 +13,7 @@
 #endif
 
 int main(void){
-	enum states {START, WAIT0, SWITCH, WAIT1} state;
+	enum states {START, WAIT, SWITCH} state;
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	state = START;
@@ -22,41 +22,37 @@ int main(void){
 		switch(state){
 			case START:
 				if((PINA & 0x01) && 0x01){
-					state = WAIT0;}
+					PORTB = 0x02;
+					state = WAIT;}
 				else{
 					state = START;}
 				break;
-			case WAIT0:
+			case WAIT:
 				if(!PINA){
 					state = SWITCH;}
 				else{
-					state = WAIT0;}
+					state = WAIT;}
 				break;
 			case SWITCH:
 				if((PINA & 0x01) && 0x01){
-					state = WAIT1;}
+					PORTB = 0x01;
+					state = WAIT;}
 				else{
 					state = SWITCH;}
 				break;
-			case WAIT1:
-				if(!PINA){
-					state = START;}
-				else{
-					state = WAIT1;}
-				break;
 		}
-		switch(state){
-			case START:
-				PORTB = 0x01;
-				break;
-			case WAIT0:
-				break;
-			case SWITCH:
-				PORTB = 0x02;
-				break;
-			case WAIT1:
-				break;
-		}
+		// switch(state){
+		// 	case START:
+		// 		PORTB = 0x01;
+		// 		break;
+		// 	case WAIT0:
+		// 		break;
+		// 	case SWITCH:
+		// 		PORTB = 0x02;
+		// 		break;
+		// 	case WAIT1:
+		// 		break;
+		// }
 	}
 	return 0;
 }
