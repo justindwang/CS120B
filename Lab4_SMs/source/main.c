8254668a016c2c5dab9c2cf292a46236004c042e
justindwang <justindwang@yahoo.com>
2021-04-12 21:37:42 -0700

    Debugging

--- Lab4_SMs/source/main.c
+++ Lab4_SMs/source/main.c
@@ -12,78 +12,47 @@
 #include "simAVRHeader.h"
 #endif
 
-enum states {START, LOCKED, WAIT_Y, UNLOCKED} state;
-unsigned char X = 0x00;
-unsigned char Y = 0x00;
-unsigned char pound = 0x00;
-unsigned char tmpA7 = 0x00;
-unsigned char tmpB = 0x00;
-unsigned char already_pressed = 0x00;
-
-void tick(void){
-	X = PINA & 0x01;
-	Y = PINA & 0x02;
-	pound = PINA & 0x04;
-	tmpA7 = PINA & 0x80;
-
-	switch(state){
-		case START:
-			state = LOCKED;
-			break;
-		case LOCKED:
-			if(X || Y || tmpA7){
-				state = LOCKED;}
-			else if(pound && !Y){
-				state = WAIT_Y;}
-			else{
-				state = LOCKED;}
-			break;
-		case WAIT_Y:
-			if(X || tmpA7){
-				state = LOCKED;}
-			else if(Y && pound){
-				state = LOCKED;}
-			else if (!Y && pound){
-				state = WAIT_Y;}
-			else if(!Y && !pound){
-				state = WAIT_Y;}
-			else if(Y && !pound){
-				state = UNLOCKED;}
-			else{
-				state = LOCKED;}
-			break;
-		case UNLOCKED:
-			if(tmpA7){
-				state = LOCKED;}
-			else{
-				state = UNLOCKED;}
-			break;
-		default:
-			break;
-	}
-	switch(state){
-		case START:
-			break;
-		case LOCKED:
-			tmpB = 0;
-			break;
-		case WAIT_Y:
-			break;
-		case UNLOCKED:
-			tmpB = 1;
-			break;
-		default:
-			break;
-	}
-}
-
 int main(void){
+	enum states {START, WAIT, SWITCH} state;
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	state = START;
+	PORTB = 0x01;
 	while(1){
-		tick();
-		PORTB = tmpB;
+		switch(state){
+			case START:
+				if((PINA & 0x01) && 0x01){
+					PORTB = 0x02;
+					state = WAIT;}
+				else{
+					state = START;}
+				break;
+			case WAIT:
+				if(!PINA){
+					state = SWITCH;}
+				else{
+					state = WAIT;}
+				break;
+			case SWITCH:
+				if((PINA & 0x01) && 0x01){
+					PORTB = 0x01;
+					state = WAIT;}
+				else{
+					state = SWITCH;}
+				break;
+		}
+		// switch(state){
+		// 	case START:
+		// 		PORTB = 0x01;
+		// 		break;
+		// 	case WAIT0:
+		// 		break;
+		// 	case SWITCH:
+		// 		PORTB = 0x02;
+		// 		break;
+		// 	case WAIT1:
+		// 		break;
+		// }
 	}
 	return 0;
 }
