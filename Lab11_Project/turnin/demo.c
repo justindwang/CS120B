ad4ccdd6a16963e4e127f315adedd1dc88c2a003
Justin <justindwang@yahoo.com>
2021-06-02 19:58:04 -0700

    Working Demo

---
+++ Lab11_Project/turnin/demo.c
@@ -0,0 +1,148 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1ms
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B 	= 0x0B;	// bit3 = 1: CTC mode (clear timer on compare)
+					// bit2bit1bit0=011: prescaler /64
+					// 00001011: 0x0B
+					// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+					// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A 	= 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+					// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+					// So when TCNT1 register equals 125,
+					// 1 ms has passed. Thus, we compare to 125.
+					// AVR timer interrupt mask register
+
+	TIMSK1 	= 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1 = 0;
+
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+	_avr_timer_cntcurr = _avr_timer_M;
+
+	//Enable global interrupts
+	SREG |= 0x80;	// 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B 	= 0x00; // bit3bit2bit1bit0=0000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect)
+{
+	// CPU automatically calls when TCNT0 == OCR0 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; 			// Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { 	// results in a more efficient compare
+		TimerISR(); 				// Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+typedef struct task{
+	// Tasks should have members that include: state, period,
+	//a measurement of elapsed time, and a function pointer.
+	signed 	 char state; 		//Task's current state
+	unsigned long period; 		//Task period
+	unsigned long elapsedTime; 	//Time elapsed since last task tick
+	int (*TickFct)(int); 		//Task tick function
+} task;
+
+
+enum Demo_States {shift};
+int Demo_Tick(int state) {
+
+	// Local Variables
+	static unsigned char pattern = 0x80;	// LED pattern - 0: LED off; 1: LED on
+	static unsigned char row = 0xFE;  	// Row(s) displaying pattern.
+							// 0: display pattern on row
+							// 1: do NOT display pattern on row
+	// Transitions
+	switch (state) {
+		case shift:
+			break;
+		default:
+			state = shift;
+			break;
+	}
+	// Actions
+	switch (state) {
+		case shift:
+			if (row == 0xEF && pattern == 0x01) { // Reset demo
+				pattern = 0x80;
+				row = 0xFE;
+			} else if (pattern == 0x01) { // Move LED to start of next row
+				pattern = 0x80;
+				row = (row << 1) | 0x01;
+			} else { // Shift LED one spot to the right on current row
+				pattern >>= 1;
+			}
+			break;
+		default:
+			break;
+	}
+	PORTC = pattern;	// Pattern to display
+	PORTD = row;		// Row(s) displaying pattern
+	return state;
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;	
+    /* Insert your solution below */
+	task tasks[1];
+	tasks[0].state = shift;
+	tasks[0].period = 200;
+	tasks[0].elapsedTime = 200;
+	tasks[0].TickFct = &Demo_Tick;
+
+	TimerSet(1);
+	TimerOn();
+
+	unsigned short i;
+    while(1) {
+	    for(i=0; i < 1; i++){
+		    if(tasks[i].elapsedTime >= tasks[i].period){
+			    tasks[i].state = tasks[i].TickFct(tasks[i].state);
+			    tasks[i].elapsedTime = 0;
+		    }
+		    tasks[i].elapsedTime += 1;
+	    }
+	    while(!TimerFlag);
+	    TimerFlag = 0;
+    }
+    return 1;
+}
